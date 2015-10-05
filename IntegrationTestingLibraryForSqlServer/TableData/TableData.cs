8a4809f8e167ab327622031ff5447d08d807ffd8
andywilsonuk <andy.wilson@zcircles.com>
2015-10-05 11:29:19 +0100

    Added missing TableData files

---
+++ IntegrationTestingLibraryForSqlServer/TableData/TableData.cs
@@ -0,0 +1,31 @@
+﻿using IntegrationTestingLibraryForSqlServer.TableDataComparison;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace IntegrationTestingLibraryForSqlServer
+{
+    public class TableData
+    {
+        public TableData()
+        {
+            this.ColumnNames = new List<string>();
+            this.Rows = new List<IList<object>>();
+        }
+
+        public IList<string> ColumnNames { get; set; }
+        public IList<IList<object>> Rows { get; set; }
+
+        public bool IsMatch(TableData other, TableDataComparer comparer)
+        {
+            return comparer.IsMatch(this, other);
+        }
+
+        public bool IsMatch(TableData other, TableDataComparers strategy)
+        {
+            return this.IsMatch(other, new TableDataComparerStrategyFactory().Comparer(strategy));
+        }
+    }
+}
---
+++ IntegrationTestingLibraryForSqlServer/TableData/TableDataComparison/ValueCompare/TableDataCaseSensitiveStringValueComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntegrationTestingLibraryForSqlServer.TableDataComparison
+{
+    public class TableDataCaseSensitiveStringValueComparer : TableDataValueComparer
+    {
+        public bool IsMatch(object x, object y)
+        {
+            if (x == null && y == null) return true;
+            if (x == null || y == null) return false;
+            return string.Equals(x.ToString(), y.ToString());
+        }
+    }
+}
