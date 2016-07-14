3b7a5a00ef3b52e6ccf65bb079aa8ab514b3c553
andywilsonuk <andy.wilson@zcircles.com>
2016-07-14 16:52:52 +0100

    Started work on ProcedureParameterCollection

---
+++ IntegrationTestingLibraryForSqlServer/Procedures/Parameters/ProcedureParameterCollection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTestingLibraryForSqlServer
+{
+    internal class ProcedureParameterCollection : Collection<ProcedureParameter>
+    {
+        private ProcedureParameterNameEqualityComparer comparer = new ProcedureParameterNameEqualityComparer();
+
+        public ProcedureParameterCollection() : base()
+        {
+        }
+
+        public ProcedureParameterCollection(IList<ProcedureParameter> list) : base(list)
+        {
+        }
+
+        protected override void InsertItem(int index, ProcedureParameter item)
+        {
+            if (Items.Any(x => comparer.Equals(x, item)))
+                throw new ArgumentException($"A parameter with the same name '{item.QualifiedName}' already exists", nameof(item));
+            base.InsertItem(index, item);
+        }
+
+        public IEnumerable<ProcedureParameter> ExcludingReturnValue
+        {
+            get { return Items.Where(x => x.Direction != ParameterDirection.ReturnValue); }
+        }
+    }
+}
---
+++ IntegrationTestingLibraryForSqlServer/Procedures/Parameters/ProcedureParameterNameEqualityComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTestingLibraryForSqlServer
+{
+    internal class ProcedureParameterNameEqualityComparer : IEqualityComparer<ProcedureParameter>
+    {
+        public bool Equals(ProcedureParameter x, ProcedureParameter y)
+        {
+            return string.Equals(x?.QualifiedName, y?.QualifiedName, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        public int GetHashCode(ProcedureParameter parameter)
+        {
+            if (parameter == null) throw new ArgumentNullException(nameof(parameter));
+            return parameter.QualifiedName.GetHashCode();
+        }
+    }
+}
